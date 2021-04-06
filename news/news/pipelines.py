56dd2efb902fb1639cc955b4287c753754d9395c
SiHe <1611729317@qq.com>
2021-04-06 09:14:52 +0800

    1

--- news/news/pipelines.py
+++ news/news/pipelines.py
@@ -13,16 +13,24 @@ from itemadapter import ItemAdapter
 class NewsPipeline(object):
 
     def process_item(self, item, spider):
-        mydb = mysql.connector.connect(host="121.4.191.75",user="root",passwd="!Zz1995711",database="spider")
+        mydb = mysql.connector.connect(host="121.4.191.75", user="root", passwd="!Zz1995711", database="spider")
         mycursor = mydb.cursor()
 
-        thisTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        sql = "SELECT * FROM sites ORDER BY name DESC"
 
-        sql = "INSERT INTO cnforex_bjdp (title,author, body,md5,create_time,update_time) VALUES (%s, %s, %s, %s, %s, %s)"
-        val = (item['mztj_title'], item['mztj_author'], item['mztj_article'], hashlib.md5(item['mztj_title'].encode(encoding='UTF-8')).hexdigest(), thisTime, thisTime)
-        mycursor.execute(sql, val)
+        mycursor.execute(sql)
 
-        mydb.commit()
+        myresult = mycursor.fetchall()
+
+        if len(myresult) <= 0:
+            thisTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            sql = "INSERT INTO cnforex_bjdp (title,author, body,md5,create_time,update_time) VALUES (%s, %s, %s, %s, %s, %s)"
+            val = (item['mztj_title'], item['mztj_author'], item['mztj_article'],
+                   hashlib.md5(item['mztj_title'].encode(encoding='UTF-8')).hexdigest(), thisTime, thisTime)
+            mycursor.execute(sql, val)
+
+            mydb.commit()
 
         # with open("my_meiju.txt", 'a', encoding='utf-8') as fp:
         #     fp.write('title:' + item['mztj_title'] + 'href:' + item['mztj_href'] + '\n' + item['mztj_article'] + '\n')
